88507a3dc586035a291a647035ebd1306c107b5e
Elad Zucker <elad-zucker@abra-it.com>
2022-05-15 12:29:42 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
-// fetch('https://cat-fact.herokuapp.com/facts')
-//   .then(response => response.json())
-//   .then(data => {
 
-//   })
-//   .catch(err => {
-//   })
+// const myFetchCalls = async() => {
+//   try {
+//     const response = await fetch('https://cat-fact.hejijirokuapp.com/facts');
+//     const data = await response.json();
+  
+//     console.log(data);
+//   } catch(e) {
+//     console.log("Error = " + e);
+//   }
+// }
 
-const mySetTimeout = ( timeout ) => {
-  return new Promise((resolve, reject) => {
+//myFetchCalls();
 
-  });
-}
 
-mySetTimeout(1000)
-  .then(() => {console.log("timer expired")})
-  .catch(() => {console.log("error in timer")})
 
-  const returnPromise = () => {
-    let myPromise = new Promise((resolve, reject) => {
-      reject(10);
-    })
+// const mySetTimeout = ( timeout ) => {
+//   return new Promise((resolve, reject) => {
+//     if (timeout < 0) {
+//       reject("Timeout value is negative");
+//       return;
+//     }
 
-    return myPromise;
-  } 
+//     setTimeout(() => { resolve("success") }, timeout );
+//   });
+// }
 
-  returnPromise()
-  .then(data => console.log("data = " + data))
-  .catch(err => console.log("error = " + err));
+// // mySetTimeout(-1000)
+// //   .then(() => {console.log("timer expired")})
+// //   .catch(() => {console.log("error in timer")})
+
+// const myFunction = async () => {
+//   console.log("Function start");
+//   try {
+//     const response = await mySetTimeout(-1000);
+//   } catch(e) {
+//     console.log(e);
+
+//   }
+//   // console.log(response);
+// }
+
+//   myFunction();
+//   // returnPromise()
+//   // .then(data => console.log("data = " + data))
+//   // .catch(err => console.log("error = " + err));
 
 
 function App() {
+
+  const getNewImageURL = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    console.log(data.message);
+
+  }
+
+  getNewImageURL();
 
   return (
     <div className="App">
