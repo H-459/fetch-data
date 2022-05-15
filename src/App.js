3863b926a1d41063eb5e8db968b0421e7b0c8cc6
Elad Zucker <elad-zucker@abra-it.com>
2022-05-15 11:50:57 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+// fetch('https://cat-fact.herokuapp.com/facts')
+//   .then(response => response.json())
+//   .then(data => {
+
+//   })
+//   .catch(err => {
+//   })
+
+const mySetTimeout = ( timeout ) => {
+  return new Promise((resolve, reject) => {
+
+  });
+}
+
+mySetTimeout(1000)
+  .then(() => {console.log("timer expired")})
+  .catch(() => {console.log("error in timer")})
+
+  const returnPromise = () => {
+    let myPromise = new Promise((resolve, reject) => {
+      reject(10);
+    })
+
+    return myPromise;
+  } 
+
+  returnPromise()
+  .then(data => console.log("data = " + data))
+  .catch(err => console.log("error = " + err));
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>test</p>
     </div>
   );
 }
